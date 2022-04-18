0763fdcb3e7d725429af5d195abf4272090c81cf
nikitkuzi <nikitkuzi@gmail.com>
2022-04-18 11:03:19 +0300

    spam prevention

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ if __name__=="__main__":
         vk_session = vk_api.VkApi(token=f.read())
     
     vk = vk_session.get_api()
-    msg = """message to post"""
+    msg = """msg"""
     myId = vk.users.get()[0]['id']
     
     # group_ids - short name
     # Example: https://vk.com/repetitorovnet
-    group_ids = ["id1", "id2"]
+    group_ids = ["repetitorovnet", "club12150271", "repetitorysimferopol", "my.tutors", "home_tutor", "my.tutor", "club153005273", "tvoyprepod", "repetitor_moscow_77", "club23026278", "repetitory_rus"]
     groupsId_to_send = []
 
     # Create array of groups to send post to
@@ -21,12 +21,24 @@ if __name__=="__main__":
     # find photo id to send, where the first photo in the list is the last you added to your albums
     # for photoes in vk.photos.getAll(owner_id=myId)['items']:
         # print("id:", photoes['id'], "url:", photoes['sizes'][0]['url'])
-    photoesId_to_send = ["id1"]
+    photoesId_to_send = ["457241095"]
 
     # send post
     for groups in groupsId_to_send:
+        spam = False
         try:
-            vk.wall.post(owner_id=groups*-1, message=msg, attachments="photo"+str(myId)+"_"+str(photoesId_to_send[0]))
+            # spam prevention
+            for count, post in enumerate(vk.wall.get(owner_id=groups*-1)['items']):
+                # post from owner
+                if post["from_id"] < 0:
+                    continue
+                # same post. better to check id
+                if post['text'] == msg:
+                    spam = True
+                if count > 5:
+                    break
+            if not spam:
+                vk.wall.post(owner_id=groups*-1, message=msg, attachments="photo"+str(myId)+"_"+str(photoesId_to_send[0]))
             time.sleep(0.5)
         except e:
             print(e)
